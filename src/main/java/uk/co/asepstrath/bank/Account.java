a7d5428335379167ab0d59b8d22dd100a71b2944
Ben <benjamin.talbot.2022@uni.strath.ac.uk>
2024-01-24 16:07:28 +0000

    1st Commit!

--- src/main/java/uk/co/asepstrath/bank/Account.java
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -1,14 +1,24 @@
 package uk.co.asepstrath.bank;
 
 public class Account {
+    private int balance;
 
+    public Account() {
+        balance = 0;
+    }
 
     public void deposit(int amount) {
+        balance = balance + amount;
+    }
 
+    public void withdraw(int amount) throws ArithmeticException{
+        if(balance - amount < 0) {
+            throw new ArithmeticException("Not enough funds available.");
+        }
+        balance = balance - amount;
     }
 
     public int getBalance() {
-        return 0;
+        return balance;
     }
-
 }
--- src/test/java/uk/co/asepstrath/bank/AccountTests.java
+++ src/test/java/uk/co/asepstrath/bank/AccountTests.java
@@ -4,12 +4,41 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AccountTests {
-
     @Test
     public void createAccount(){
         Account a = new Account();
         Assertions.assertTrue(a != null);
     }
 
+    @Test
+    public void checkNewBalance(){
+        Account a = new Account();
+        Assertions.assertTrue(a.getBalance() == 0);
+    }
 
+    @Test
+    public void depositMoney(){
+        Account a = new Account();
+        a.deposit(20);
+        Assertions.assertTrue(a.getBalance() == 20);
+        a.deposit(50);
+        Assertions.assertTrue(a.getBalance() == 70);
+    }
+
+    @Test
+    public void withdrawMoney(){
+        Account a = new Account();
+        a.deposit(40);
+        Assertions.assertTrue(a.getBalance() == 40);
+        a.withdraw(20);
+        Assertions.assertTrue(a.getBalance() == 20);
+    }
+
+    @Test
+    public void overdraftTest() {
+        Account a = new Account();
+        a.deposit(30);
+        Assertions.assertTrue(a.getBalance() == 30);
+        Assertions.assertThrows(ArithmeticException.class, () -> a.withdraw(100));
+    }
 }
