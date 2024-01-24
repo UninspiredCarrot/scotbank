77eeda8738988bad4c3a43094322c9df950ef4e5
Morven <morvenfarquharson23@gmail.com>
2024-01-24 16:32:19 +0000

    rounding code

--- src/main/java/uk/co/asepstrath/bank/Account.java
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -1,14 +1,37 @@
 package uk.co.asepstrath.bank;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Account {
 
+    private double balance = 0.0;
 
-    public void deposit(int amount) {
-
+    public Account (double amount) {
+        balance = amount;
     }
 
-    public int getBalance() {
-        return 0;
+    public void deposit(double amount) {
+        balance += amount;
+        balance = round();
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void withdraw(double amount) {
+
+        if (amount > balance) throw new ArithmeticException();
+
+        balance -= amount;
+        balance = round();
+    }
+
+    public double round() {
+        BigDecimal rn = new BigDecimal(Double.toString(balance));
+        rn = rn.setScale(2, RoundingMode.HALF_UP);
+        return rn.doubleValue();
     }
 
 }
--- src/test/java/uk/co/asepstrath/bank/AccountTests.java
+++ src/test/java/uk/co/asepstrath/bank/AccountTests.java
@@ -1,4 +1,5 @@
 package uk.co.asepstrath.bank;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -7,9 +8,51 @@ public class AccountTests {
 
     @Test
     public void createAccount(){
-        Account a = new Account();
-        Assertions.assertTrue(a != null);
+        Account a = new Account(0);
+        assertTrue(a != null);
+        Assertions.assertTrue(a.getBalance() == 0);
     }
 
+    @Test
+    public void accountDeposits(){
+        Account a = new Account(20);
+        a.deposit(50);
+        assertTrue(a.getBalance() == 70);
+
+    }
+
+    @Test
+    public void accountWithdrawls() {
+        Account a = new Account(40);
+        a.withdraw(20);
+        assertTrue(a.getBalance() == 20);
+
+        Account b = new Account(30);
+        assertThrows(ArithmeticException.class,() -> b.withdraw(100));
+    }
+
+    @Test
+    public void superSaving() {
+        Account a = new Account(20);
+        a.deposit(10);
+        a.deposit(10);
+        a.deposit(10);
+        a.deposit(10);
+        a.deposit(10);
+
+        a.withdraw(20);
+        a.withdraw(20);
+        a.withdraw(20);
+
+        assertTrue(a.getBalance() == 10);
+
+    }
+
+    @Test
+    public void pennies() {
+        Account a = new Account(5.45);
+        a.deposit(17.56);
+        assertEquals(23.01, a.getBalance());
+    }
 
 }
