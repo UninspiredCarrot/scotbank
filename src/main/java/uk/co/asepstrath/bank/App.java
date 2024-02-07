51a7d0fd0cd3323d2c3a714565aeb0b71273fffc
rtb23126 <james.park.2023@uni.strath.ac.uk>
2024-02-07 23:54:17 +0000

    Transaction and Account API implemented and tables created
    Transaction table populated, Account table populated, Still to implement USER table for use with login process

--- src/main/java/uk/co/asepstrath/bank/App.java
+++ src/main/java/uk/co/asepstrath/bank/App.java
@@ -1,7 +1,17 @@
 package uk.co.asepstrath.bank;
 
+import kong.unirest.core.GenericType;
 import kong.unirest.core.HttpResponse;
+import kong.unirest.core.JsonNode;
 import kong.unirest.core.Unirest;
+import kong.unirest.core.json.JSONArray;
+import kong.unirest.core.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import uk.co.asepstrath.bank.example.ExampleController;
 import io.jooby.Jooby;
 import io.jooby.handlebars.HandlebarsModule;
@@ -10,10 +20,19 @@ import io.jooby.hikari.HikariModule;
 import org.slf4j.Logger;
 
 import javax.sql.DataSource;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App extends Jooby {
 
@@ -71,26 +90,65 @@ public class App extends Jooby {
             stmt.executeUpdate("INSERT INTO Example " + "VALUES ('WelcomeMessage', 'Welcome to A Bank')");
 
             //---------------Testing transaction table plus data insertion--------------------------
-            stmt.executeUpdate(
-                    "CREATE TABLE IF NOT EXISTS`transactions` (" +
-                    "id VARCHAR(255) PRIMARY KEY,"+
-                    "`to` VARCHAR(255) NOT NULL," +
-                    "amount DECIMAL NOT NULL,"+
-                    "transaction_type VARCHAR(255) NOT NULL"+
-                    ")"
-                );
             String sql = (
-                    "INSERT INTO transactions (" +
-                    "id, `to`, amount, transaction_type" +
-                    ")"+
-                    "VALUES (?,?,?,?)"
+                            "CREATE TABLE IF NOT EXISTS`transactions` (" +
+                            "id VARCHAR(255) PRIMARY KEY,"+
+                            "timestamp VARCHAR(255) NOT NULL,"+
+                            "`to` VARCHAR(255) NOT NULL," +
+                            "amount DECIMAL NOT NULL,"+
+                            "transaction_type VARCHAR(255) NOT NULL"+
+                            ")"
             );
-            PreparedStatement prep = connection.prepareStatement(sql);
-            prep.setString(1, "test_id");
-            prep.setString(2, "test_id_to");
-            prep.setDouble(3, 50);
-            prep.setString(4, "Deposit");
-            prep.executeUpdate();
+
+            stmt.executeUpdate(sql);
+
+//            StringReader sr = new StringReader(
+//                    Unirest
+//                            .get("https://api.asep-strath.co.uk/api/transactions").asString().getBody()
+//            );
+
+            URL url = new URL("https://api.asep-strath.co.uk/api/transactions");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(new InputSource(url.openStream()));
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("results");
+            ArrayList<Transaction> transactions = new ArrayList<>();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+
+                Node child = nodeList.item(i).getFirstChild();
+
+                String timestamp = child.getFirstChild().getNodeValue();
+                child = child.getNextSibling();
+
+                double amount = Double.parseDouble(child.getFirstChild().getNodeValue());
+                child = child.getNextSibling();
+
+                String id = child.getFirstChild().getNodeValue();
+                child = child.getNextSibling();
+
+                String to = child.getFirstChild().getNodeValue();
+                child = child.getNextSibling();
+
+                String type = child.getFirstChild().getNodeValue();
+                child = child.getNextSibling();
+
+                sql = (
+                        "INSERT INTO transactions (" +
+                                "id, `to`, timestamp, amount, transaction_type" +
+                                ")"+
+                                "VALUES (?,?,?,?,?)"
+                );
+                PreparedStatement prep = connection.prepareStatement(sql);
+                prep.setString(1, id);
+                prep.setString(2, to);
+                prep.setString(3, timestamp);
+                prep.setDouble(4, amount);
+                prep.setString(5, type);
+                prep.executeUpdate();
+            }
+
             //---------------------------------------------------------------------------------------
 
             //------------get user information from api and save to a users table-----------------
@@ -103,10 +161,10 @@ public class App extends Jooby {
                     );
             stmt.executeUpdate(sql);
 
-            HttpResponse<Account[]> accountResponse =
+            HttpResponse<List<Account>> accountResponse =
                     Unirest
                     .get("https://api.asep-strath.co.uk/api/accounts")
-                    .asObject(Account[].class);
+                    .asObject(new GenericType<>(){});
 
             for(Account account : accountResponse.getBody()){
                 sql = (
@@ -114,21 +172,29 @@ public class App extends Jooby {
                     "id, name, balance, round_up_enabled" +
                     ")VALUES (?,?,?,?);"
                 );
-                prep = connection.prepareStatement(sql);
+                PreparedStatement prep = connection.prepareStatement(sql);
                 prep.setString(1, account.getId());
                 prep.setString(2, account.getName());
                 prep.setDouble(3, account.getBalance());
                 prep.setBoolean(4, account.isRoundUpEnabled());
                 prep.executeUpdate();
-//                log.info(
-//                        String.format(
-//                                "{%s} - added to database", account
-//                        )
-//                );
+                log.info(
+                        String.format(
+                                "{%s} - added to database", account
+                        )
+                );
             }
             //---------------------------------------------------------------------------------------
         } catch (SQLException e) {
             log.error("Database Creation Error",e);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
+            throw new RuntimeException(e);
         }
     }
 
